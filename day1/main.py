3fb07e45168e696c08892c1ed0528941b6619f76
Vanessa Roque <viroque19@gmail.com>
2023-12-01 12:09:34 +0000

    day 1 solution

--- day1/main.py
+++ day1/main.py
@@ -1,28 +1,55 @@
-digits = ["notvaliddigits", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+def part2(line, tot):
+    curr = ""
+    nums = ""
+    digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for c in line:
+        curr += c
+        if c.isdigit():
+            nums += c
+            curr = ""
+        for i in range(len(digits)):
+            if digits[i] in curr:
+                nums += str(i+1)
+                curr = curr[len(curr)//2:]
+    if len(nums) == 1:
+        nums = nums*2
+    n = len(nums)
+    curr = ""
+    curr += str(nums[0])
+    curr += str(nums[n-1])
+    # print(f"(current){curr} +(previous){tot}")
+    return int(curr)
 
-with open("input.txt", "r") as f:
-    total = 0
-    for line in f:
-        curr = "" 
-        line.rstrip()
-        nums = ""
+def part1(line):
+    curr = ""
+    nums = ""
+    for c in line:
+        curr += c
+        if c.isdigit():
+            nums += c
+            curr = ""
+    if len(nums) == 1:
+        nums = nums*2
+    n = len(nums)
+    curr = ""
+    curr += str(nums[0])
+    curr += str(nums[n-1])
+    return int(curr)
+
+def main():
+    with open("input.txt", "r") as f:
+        total = 0
+        for line in f:
+            curr = "" 
+            line.rstrip()
+            nums = ""
+            # total += part1(line)
+            print(line)
+            total += part2(line, total)
+            print(total)
+            
+        print(total)
         
-        for c in line:
-            curr += c
-            for i in range(len(digits)):
-                if str(digits[i]) in curr:
-                    nums += str(i)
-                    curr = curr[1:len(curr)]
-            if c.isdigit():
-                nums += c
-                curr = ""
-        if len(nums) == 1:
-            nums = nums*2
-        n = len(nums)
-        curr = ""
-        curr += str(nums[0])
-        curr += str(nums[n-1])
-        # print(line, nums[0], nums[n-1], total)
-
-        total += int(curr)
-    print(total)
+if __name__ == "__main__":
+    main()
+                
