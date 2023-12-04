1d5584f36bce1f579bd0539709e345e3d61456ae
Vanessa Roque <viroque19@gmail.com>
2023-12-04 10:33:59 +0000

    day 4 pt 2 solution

--- day4/main2.py
+++ day4/main2.py
@@ -1,11 +1,9 @@
 import re
 def set_game_freq(hmap, length):
     for i in range(1, length):
-        hmap[i] = 1
+        hmap[i] = 1 # start with 1 card for each game
     return hmap
-def get_curr_total(hmap, count):
-    return 0
-    ...
+
 def get_winning_nums(winning_nums, my_nums):
     my_wins = []
     for num in my_nums:
@@ -14,15 +12,10 @@ def get_winning_nums(winning_nums, my_nums):
     return my_wins
 
 def update_game_freq(cards, curr_card, card_freq):
-    # print("game:", curr_card)
-    # print("duplicates:", cards)
-    # count = card_freq[curr_card]
     for i in range(curr_card+1, curr_card+1+cards):
         card_freq[i] += 1
     return card_freq
 
-
-# 28276 not correct
 def main():
     with open("input2.txt", 'r') as f:
         lines = f.readlines()
@@ -40,17 +33,14 @@ def main():
             my_nums = list(map(int, matches2))
             my_wins = get_winning_nums(winning_nums, my_nums)
             duplicates = len(my_wins)
-
+            # update the freq table if duplicates
             if duplicates > 0:
                 print(f"adding {duplicates}cards from game{game_num}")
                 for i in range(game_freq[game_num]):
                     update_game_freq(duplicates, game_num, game_freq)
-
             total += game_freq[game_num]
             game_num += 1
-        # print(game_freq)
         print(total)
-
 
 if __name__ == "__main__":
     main()
