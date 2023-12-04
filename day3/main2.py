a1d619d8eb7f70432d1144f0dd82fadd6028854e
Vanessa Roque <viroque19@gmail.com>
2023-12-04 01:22:53 +0000

    got day 2 🤦

--- day3/main2.py
+++ day3/main2.py
@@ -1,18 +1,18 @@
 def is_symbol(char):
     return char in "#$+=*-&/@%"
 
-def is_valid_part(string, input, valid_pos, r, c, length, gears):
+def is_valid_part(string, input, valid_pos, r, c, length, gears, num):
     for i in range(length, 0, -1):
-        # print(valid_pos[r][c-i], input[r][c-i])
-        if valid_pos[r][c-i] != 0:
-            if valid_pos[r][c-1] not in gears:
-                gears[valid_pos[r][c-i]] = int(string)
+        key = valid_pos[r][c-i]
+        if key == num:
+            if key not in gears:
+                gears[key] = int(string)
                 return False
             else:
-                return True
-    # print(gears)
+                val = gears[key]
+                # print(val, string)
+                return True # there are two neighbors
     
-        # print(valid_pos[r][c-i], input[r][c-i])
 
 def print_marked_pos(adj_list):
     for row in adj_list:
@@ -33,43 +33,34 @@ def main():
         total = 0
         num = 1
         
-        gears = {}
-        # print(height, width)
-        mark = [[0 for i in range(width)]for i in range(height)]
-        # print(mark)
+        gears = {} ## initialize dict to hold values of neighbors
+        ###
+        mark = [[0 for i in range(width)]for i in range(height)] # mark all neighbors
         for r, line in enumerate(lines):
             for c, char in enumerate(line):
                 ratio = 0
                 if is_symbol(char):
                     mark_neighbors(mark, r, c, height, width, num)
-                        ###
+        ###
                     # analyze all the numbers right after marking current neighbors
                     for i in range(height):
                         curr_part = ""
                         for j in range(width+1):
-                            # print(j)
-                            # print(lines[i],end="")
                             char = lines[i][j]
                             if char.isdigit():
                                 curr_part += char
                             if not char.isdigit() and curr_part or j == width and curr_part:
-                                # print(curr_part)
                                 length = len(curr_part)
-                                valid = is_valid_part(curr_part, lines, mark, i, j, length, gears)
-                                # print(curr_part, valid)
+                                valid = is_valid_part(curr_part, lines, mark, i, j, length, gears, num)
                                 if valid:
+                                    ratio = gears[num]*int(curr_part)
+                                    total += ratio
                                     # print(curr_part, valid)
-                                    # ratio = int(curr_part) * gears[mark[r][c]]
-                                    print(gears)
-                                    # ratio = 1
                                 curr_part = ""
-                    ###
+        ###
                     num += 1
-                
-            # print(line, end="")
-        
-        print_marked_pos(mark)
-        print(gears)
+
+        # print(gears)
     print(total)
 
 if __name__ == "__main__":
