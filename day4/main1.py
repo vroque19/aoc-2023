f9630a82d24d61c6170b28789f7315a34a00d5e6
Vanessa Roque <viroque19@gmail.com>
2023-12-04 08:21:34 +0000

    pt1 solution

--- day4/main1.py
+++ day4/main1.py
@@ -1,6 +1,13 @@
 import re
 
 # 28276 not correct
+def get_winning_nums(winning_nums, my_nums):
+    my_wins = []
+    for num in my_nums:
+        if num in winning_nums:
+            my_wins.append(num)
+    return my_wins
+
 def main():
     with open("input1.txt", 'r') as f:
         lines = f.readlines()
@@ -14,11 +21,8 @@ def main():
 
             winning_nums = list(map(int, matches1))
             my_nums = list(map(int, matches2))
-            my_wins = []
+            my_wins = get_winning_nums(winning_nums, my_nums)
             
-            for num in my_nums:
-                if num in winning_nums:
-                    my_wins.append(num)
             if len(my_wins) > 0:
                 total += 2**(int(len(my_wins)-1))
             else: total += 0
